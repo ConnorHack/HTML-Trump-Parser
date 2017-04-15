fd10d27a889a9ef2b190d94e219666a7d1c5b177
Daniel Cormier <drc5640@rit.edu>
2017-04-15 17:48:54 -0400

    made the script able to iterate over a file, cnn.txt, that contains cnn links on each line added some comments

--- scrape.py
+++ scrape.py
@@ -1,37 +1,62 @@
-import urllib.request
+# extract article title and remove html tags
 import re
+# get text from specific html tags
 from bs4 import BeautifulSoup
+# get the web page content
 import requests
+# string.punctuation
 import string
+# write the output csv file
 import csv
 
-url = 'http://www.cnn.com/2017/04/14/politics/donald-trump-north-korea-mar-a-lago/index.html'
-title = re.sub('.*/(.*)/index\.html',"\g<1>",url)
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-links = soup.select('div.zn-body__paragraph')
-strlist = []
-freq = {}
-exclude = set(string.punctuation)
-exclude.discard("'")
-exclude.discard('-')
-for lstring in links:
-    newstr = str(lstring)
-    newstr = re.sub('<.*?>', '', newstr)
-    s = ''.join(ch for ch in newstr if ch not in exclude)
-    strlist.append(s)
 
-for lstring in strlist:
-    for word in lstring.split():
-        w = word.lower()
-        if w in freq.keys():
-            freq[w] += 1
-        else:
-            freq[w] = 1
-with open('cnn-{}.csv'.format(title),'w') as f:
-    w = csv.writer(f)
-    for key, value in freq.items():
-        w.writerow([key,value])
+def parse_cnn( url ):
+    '''
+    parse_cnn parses a cnn webpage for the information in an article.
+    @param url the string representation of the cnn url
+    '''
+    # extract title from page url
+    title = re.sub('.*/(.*)/index\.html',"\g<1>",url)
+    # get page and make it nice
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    # grab part of page we want
+    links = soup.select('div.zn-body__paragraph')
+    strlist = []
+    # the dictionary mapping word->freqency
+    freq = {}
+    # set of punctuation to strip, not including ' and -
+    exclude = set(string.punctuation)
+    exclude.discard("'")
+    exclude.discard('-')
+    # strip html tags and punctuation
+    for lstring in links:
+        newstr = str(lstring)
+        newstr = re.sub('<.*?>', '', newstr)
+        s = ''.join(ch for ch in newstr if ch not in exclude)
+        strlist.append(s)
 
-#print(links)
-#print(soup.prettify())
+    # break into words and update freqencies
+    for lstring in strlist:
+        for word in lstring.split():
+            w = word.lower()
+            if w in freq.keys():
+                freq[w] += 1
+            else:
+                freq[w] = 1
+    # write the frequencies as a csv
+    with open('cnn-{}.csv'.format(title),'w') as f:
+        w = csv.writer(f)
+        for key, value in freq.items():
+            w.writerow([key,value])
+
+def parse_cnn_file( filename ):
+    '''
+    parse_cnn_file parses a file containing cnn links, one on each line.
+    @param filename the name of the file to read.
+    '''
+    with open(filename) as f:
+        for line in f:
+            parse_cnn( line.strip() )
+
+parse_cnn_file( "cnn.txt" )
